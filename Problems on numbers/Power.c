24fc06272b9c53dae3a277c9cbc7d6d1faf00051
AtharvaArgade <84590699+AtharvaArgade@users.noreply.github.com>
2021-05-22 00:21:20 +0530

    Update Power.c

--- Problems on numbers/Power.c
+++ Problems on numbers/Power.c
@@ -5,16 +5,6 @@
 // Input : 4 3
 // Output : 4*4*4     (64)
 
-/*
-	iNo1=5        iNo2=4       iMult=1
-	5*5*5*5
-
-	1*5		=5                            Number of iterations 4 (iNo2)
-	5*5		=25							  Loop iterates iNo2 number of times
-	25*5	=125
-	125*5	=625						  __*5(iNo1)
-										  __*iNo1
-*/
 
 #include<stdio.h>
 
