306f9cabcda9a066977d3f6a03d9089b2f0f212b
AtharvaArgade <84590699+AtharvaArgade@users.noreply.github.com>
2021-05-22 00:22:44 +0530

    Update Table.c

--- Problems on numbers/Table.c
+++ Problems on numbers/Table.c
@@ -1,17 +1,4 @@
 // Accept number from user and display table of that number
-// Input : 4
-// Output : 4 8 12 16 20 24 28 32 36 40
-
-/*
-4 * 1 = 4
-4 * 2 = 8			 
-4 * 3 = 12           common 		4*__
-4 * 4 = 16			 Start  		1
-.					 End   			10
-.					 Displacement  	1
-.
-4 * 10 = 40
-*/
 
 #include<stdio.h>
 
