c7255e157a8ac3988dfd4e6658a017be6ecd7ca1
AtharvaArgade <84590699+AtharvaArgade@users.noreply.github.com>
2021-06-25 01:20:36 +0530

    Update demo5.c

--- preprocessor/demo5.c
+++ preprocessor/demo5.c
@@ -14,19 +14,3 @@ int main()
     return 0;
 }
 
-/*
-E:\Pre_Placement_activity\parallel\preprocessor>gcc demo5.c -o demo
-demo5.c: In function 'fun':
-demo5.c:7: warning: "MARVELLOUS" redefined
-    7 |     #define MARVELLOUS 21
-      |
-demo5.c:3: note: this is the location of the previous definition
-    3 | #define MARVELLOUS 11
-      |
-
-E:\Pre_Placement_activity\parallel\preprocessor>demo.exe
-21
-21
-
-E:\Pre_Placement_activity\parallel\preprocessor>
-*/
