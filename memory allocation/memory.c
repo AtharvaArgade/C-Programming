563f05c3946d15abbdb7ae9f2e2067bd1e268567
AtharvaArgade <84590699+AtharvaArgade@users.noreply.github.com>
2021-06-25 01:08:00 +0530

    Update memory.c

--- memory allocation/memory.c
+++ memory allocation/memory.c
@@ -10,7 +10,7 @@ int main()
 {
   // Static memory allocation
 
-  int arr[5];   // 20 bytes of memory
+  int arr[5];  
 
   // Dynamic memory allocation
 
@@ -20,7 +20,7 @@ int main()
   printf("Enter number of elements that you want to allocate\n");
   scanf("%d",&size);
 
-  ptr=(int*)malloc(size * sizeof(int));   //  m alloc Memory Allocation
+  ptr=(int*)malloc(size * sizeof(int));  
   if(ptr==NULL)
   {
     printf("Unable to allocate memory\n");
@@ -29,11 +29,11 @@ int main()
   
   printf("Dynamic memory gets allocated in heap at the address %lu\n",ptr);
   printf("Ptr+1 : %lu\n",ptr+1);
-/*
+
   ptr[0]=10;
   ptr[1]=20;
   ptr[2]=30;
-*/
+
   printf("Elements are %d %d %d\n",ptr[0],ptr[1],ptr[2]);
 
   printf("%d\n",sizeof(ptr));
@@ -49,30 +49,3 @@ int main()
  
   return 0;
 }
-/*
-E:\Pre_Placement_activity\parallel\memory>gcc memory.c -o memory
-
-E:\Pre_Placement_activity\parallel\memory>memory.exe
-Enter number of elements that you want to allocate
-4
-Dynamic memory gets allocated in heap at the address 8071544
-Elements are 10 20 30
-4
-4
-Free the allocated memory
-Dynamic memory gets allocated in heap at the address 8071544
-
-E:\Pre_Placement_activity\parallel\memory>gcc memory.c -o memory
-
-E:\Pre_Placement_activity\parallel\memory>memory.exe
-Enter number of elements that you want to allocate
-5
-Dynamic memory gets allocated in heap at the address 8006008
-Elements are 7998696 8007496 -1275068236
-4
-4
-Free the allocated memory
-Dynamic memory gets allocated in heap at the address 8006008
-
-E:\Pre_Placement_activity\parallel\memory>
-*/
