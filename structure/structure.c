32ad6db2ec329cb25c7fc53dd3779eb5712f462a
AtharvaArgade <84590699+AtharvaArgade@users.noreply.github.com>
2021-06-25 01:25:41 +0530

    Update structure.c

--- structure/structure.c
+++ structure/structure.c
@@ -14,8 +14,7 @@ int main()
   struct Demo obj1;
   struct Demo obj2;
   struct Demo obj3={101,80.5,90.5};   // Member initalisation list
-  // But here we have to intialise all the elements of structure by using member initalisation list
-
+  
   // Object Initalisation           // Member by Member initalisation
   obj1.no=11;
   obj1.f=10.5;
@@ -33,15 +32,3 @@ int main()
   printf("Obj1.d is %f\n",obj1.d);
   return 0;
 }
-/*
-E:\Pre_Placement_activity\parallel\structure>gcc structure.c -o structure
-
-E:\Pre_Placement_activity\parallel\structure>structure.exe
-Size of the object obj1 is : 16
-Contents of obj1
-Obj1.no is 11
-Obj1.f is 10.50
-Obj1.d is 20.500000
-
-E:\Pre_Placement_activity\parallel\structure>
-*/
