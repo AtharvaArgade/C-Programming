715b7fbbc25ffcfb73ba9e11d125678e93a599c9
AtharvaArgade <84590699+AtharvaArgade@users.noreply.github.com>
2021-06-25 01:18:54 +0530

    Update demo2.c

--- preprocessor/demo2.c
+++ preprocessor/demo2.c
@@ -13,20 +13,3 @@ int main()
 
   return 0;
 }
-
-/*
-E:\Pre_Placement_activity\parallel\preprocessor>gcc demo2.c -o demo
-demo2.c: In function 'main':
-demo2.c:10: warning: "MARVELLOUS" redefined
-   10 |   #define MARVELLOUS 21
-      |
-demo2.c:4: note: this is the location of the previous definition
-    4 | #define MARVELLOUS 11    // user defined macro
-      |
--------------------------------------------------------------------------------
-E:\Pre_Placement_activity\parallel\preprocessor>demo.exe
-11
-21
-
-E:\Pre_Placement_activity\parallel\preprocessor>
-*/
